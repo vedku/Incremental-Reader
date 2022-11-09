38a9eab7aee1760368d57af46acbf8bbbede8644
vedku <57610056+vedku@users.noreply.github.com>
2022-11-08 22:52:50 -0800

    Create garbagedump.swift

---
+++ Swift/Initial Work/garbagedump.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+
+struct DefaultsKeys {
+    static let keyOne = "firstStringKey" 
+    static let keyTwo = "secondStringKey"
+}
+
+struct ContentView: View {
+    @State var Mytext = UserDefaults.standard.string(forKey: DefaultsKeys.keyOne) ?? "Edit this text here with the text to be incremented" 
+    @State var incrementedtext = []
+    var body: some View {
+        VStack{
+            Text("**Incremental Reader**") 
+                .font(.system(.largeTitle, design: .monospaced))
+            TextEditor(text: $Mytext)
+                .font(.system(.body, design: .monospaced))
+                .border(.mint) 
+                }
+            HStack{
+                Button("Increment!"){
+                    Mytext = ("Incremented Text")
+                    let incrementedtext = Mytext.components(separatedBy: ", ") 
+                }
+                .padding()
+                Button("Revert!"){
+                    Mytext = ("Reverted Text")
+                }
+            }
+        }
+}

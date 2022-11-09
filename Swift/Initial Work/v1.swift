3499f151f093383f42798e70ac844363b7d17e34
vedku <57610056+vedku@users.noreply.github.com>
2022-11-08 16:43:32 -0800

    Update v1.swift

--- Swift/Initial Work/v1.swift
+++ Swift/Initial Work/v1.swift
@@ -1,1 +1,34 @@
-//
+import SwiftUI
+
+struct DefaultsKeys {
+    static let keyOne = "firstStringKey" 
+    static let keyTwo = "secondStringKey"
+}
+
+struct ContentView: View {
+    @State var Mytext = UserDefaults.standard.string(forKey: DefaultsKeys.keyOne) ?? "Edit this text here with the text to be incremented" 
+    @State var CountColor: Color = .black
+    @State var CountMax: Float = 280
+    var body: some View {
+        VStack{
+            Text("**Incremental Reader**") 
+                .font(.system(.largeTitle, design: .monospaced)) //Font choice
+            TextEditor(text: $Mytext)
+                .font(.system(.body, design: .monospaced))
+                .border(.mint) 
+                .padding()
+                .onChange(of: Mytext) { 
+                    newValue in
+                    if Mytext.count > Int(CountMax) {
+                        CountColor = .red}
+                    else {CountColor = .black}
+                }
+            HStack{
+                Button("Increment!"){
+                    Mytext = ("Penis")
+                }
+                .padding()
+            }
+        }
+        
+    }

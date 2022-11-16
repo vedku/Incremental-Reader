87df1ccb01a79d171bc1030c58e977b3d3eaaa76
vedku <57610056+vedku@users.noreply.github.com>
2022-11-17 00:36:49 +0800

    Update v3_withseparator.py

--- Python/Initial/v3_withseparator.py
+++ Python/Initial/v3_withseparator.py
@@ -1,7 +1,10 @@
-import time
 txt = input("Input:")
 separator = input("Separator:")
 splitted = txt.split(separator)
-for i in range(len(splitted)):
-    print(splitted[i])
-    time.sleep(4)
+count = 0
+for j in range(len(splitted)):
+   temp = [splitted[0],splitted[1],splitted[2]]
+   if j == 0:
+        temp.pop(0)
+        temp.append("John Paul Jones")
+        print(temp)

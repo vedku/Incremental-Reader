229df8f56a1654291e998634114e382b32d8108f
vedku <57610056+vedku@users.noreply.github.com>
2022-11-17 00:40:26 +0800

    Update v2.py

--- Working Builds/v2.py
+++ Working Builds/v2.py
@@ -1,3 +1,5 @@
+import time
+
 txt = input("Input:")
 separator = input("Separator:")
 splitted = txt.split(separator)
@@ -7,9 +9,11 @@ for j in range(len(splitted)):
    if j == 0:
         temp[0] = 'XXX'
         print(temp)
+        time.sleep(4)
    elif j == 1:
        temp[1] = 'XXX'
        print(temp)
+       time.sleep(4)
    elif j == 2:
        temp[2] = 'XXX'
        print(temp)

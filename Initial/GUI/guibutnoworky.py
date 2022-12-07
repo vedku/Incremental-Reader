8844924312d1282073417516b5fe2924d035310e
vedku <57610056+vedku@users.noreply.github.com>
2022-12-06 16:51:24 -0800

    Update guibutnoworky.py

--- Initial/GUI/guibutnoworky.py
+++ Initial/GUI/guibutnoworky.py
@@ -1,29 +1,19 @@
-import tkinter as tk					
+import tkinter as tk
 from tkinter import ttk
 
 
 root = tk.Tk()
-root.title("Tab Widget")
+root.title("Incremental Reader")
 tabControl = ttk.Notebook(root)
 
 tab1 = ttk.Frame(tabControl)
 tab2 = ttk.Frame(tabControl)
 
-tabControl.add(tab1, text ='Tab 1')
-tabControl.add(tab2, text ='Tab 2')
-tabControl.pack(expand = 1, fill ="both")
+tabControl.add(tab1, text='Flashcards')
+tabControl.add(tab2, text='Study')
+tabControl.pack(expand=1, fill="both")
 
-ttk.Label(tab1,
-		text ="Welcome to \
-		GeeksForGeeks").grid(column = 0,
-							row = 0,
-							padx = 30,
-							pady = 30)
-ttk.Label(tab2,
-		text ="Lets dive into the\
-		world of computers").grid(column = 0,
-									row = 0,
-									padx = 30,
-									pady = 30)
+ttk.Label(tab1, text="This should show all of the flashcards you have made.").grid(column=0, row=0, padx=30, pady=30)
+ttk.Label(tab2, text="This should show the flashcard currently being studied").grid(column=0, row=0, padx=30, pady=30)
 
 root.mainloop()
